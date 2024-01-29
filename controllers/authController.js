d05f9db154e21c03d8906175d80afce240324d84
Renier Vargas <renier1989@gmail.com>
2024-01-28 20:00:56 -0500

    obtener y verificar el usaurio autenticado el JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,6 +43,23 @@ const autenticarUsuario = async (req, res, next) => {
   }
 };
 
-const usuarioAutenticado = async (res, req) => {};
+const usuarioAutenticado = async (req, res,next) => {
+
+    const userToken = req.get('Authorization');
+
+    if(userToken){
+        // obtenemos el token del usuario autenticado y lo verificamos
+        const token = userToken.split(' ')[1];
+
+        const usuario = jwt.verify(token, process.env.SECRET_JWT);
+        console.log(usuario);
+
+    }else{
+        console.log('El Token no es valido!');
+    }
+
+    return next();
+
+};
 
 export { autenticarUsuario, usuarioAutenticado };
