de55032064f474ecc889c6d066c6e4e29b52e33b
Renier Vargas <renier1989@gmail.com>
2024-02-02 18:10:41 -0500

    algunos ajustes para permitir las peticiones que llegan del frontend habilitando los cors

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ import usuariosRoutes from "./routes/usuarios.js"
 import authRoutes from "./routes/auth.js"
 import enlacesRoutes from "./routes/enlaces.js"
 import archivosRoutes from "./routes/archivos.js"
+import cors from 'cors'
 
 // creamos el servidor
 const app = express();
 app.use(express.json());
 // conectar con la BD
 conectarDB();
+// habilitar los CORS 
+const corsOptions = {
+  origin : process.env.FRONTEND_URL
+}
+app.use( cors(corsOptions));
 // puerto de la app
 const port = process.env.PORT || 4000;
 // definiendo las rutas de la app
